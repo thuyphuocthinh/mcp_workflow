67d0a64e79967251fed3283da362b7c41f0a571b
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2026-01-01 09:40:10 +0700

    feat(main): set global API prefix to 'api/v1'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ import { LoggingInterceptor } from '@/shared/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
